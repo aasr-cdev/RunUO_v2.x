9b0fb486911839594cebc16b759f7983c3707aa9
aasr-cdev <57951700+aasr-cdev@users.noreply.github.com>
2020-01-01 18:42:36 -0500

    Initial commit

---
+++ Server/Engines/Account.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Server.Accounting
+{
+    public interface IAccount : IComparable<IAccount>
+    {
+        string Username { get; set; }
+        AccessLevel AccessLevel { get; set; }
+
+        int Length { get; }
+        int Limit { get; }
+        int Count { get; }
+        Mobile this[int index] { get; set; }
+
+        void Delete();
+        void SetPassword(string password);
+        bool CheckPassword(string password);
+    }
+}
